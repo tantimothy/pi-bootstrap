f045eeb29a16ea6b2a92bed7be2baab693b18039
Timothy Tan <ttimothy@mac.com>
2019-07-28 17:45:30 +0800

    hassDocker Build file

--- hassDockerBuild.sh
+++ hassDockerBuild.sh
@@ -1,8 +1,2 @@
-cd ~/
-if cd ~/rpi-home-assistant; then git pull; else git clone https://github.com/lroguet/rpi-home-assistant/; fi
-
 cd /home/pi/pi-bootstrap/hassDocker
 docker build -t ttimothy/hass .
-
-cd ~/rpi-home-assistant
-./build.sh
--- runHassDocker.sh
+++ runHassDocker.sh
@@ -2,5 +2,5 @@ docker run -d \
   --name hass \
   --net=host \
   -v /etc/localtime:/etc/localtime:ro \
-  -v /home/pi/home-assistant/configuration:/config \
+  -v /home/pi/Home-AssistantConfig:/config \
   lroguet/rpi-home-assistant:latest
