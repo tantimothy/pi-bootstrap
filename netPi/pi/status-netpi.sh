40fb02c047b10219c888464b6b59cd43b6f1a539
Timothy Tan <ttimothy@mac.com>
2020-09-03 17:18:14 +0800

    Update status-netpi.sh

--- netPi/pi/status-netpi.sh
+++ netPi/pi/status-netpi.sh
@@ -1,11 +1,16 @@
 #/bin/bash
 
+pivpn -d
+
 pihole -v 
 pihole status
+
 sudo wg
 systemctl status wg-quick@wg0
+
 service darkstat status
 service ntopng status
+
 #systemctl status redis-server 
 #sudo ipsec verify
 #sudo cat /var/log/openvpn-status.log
